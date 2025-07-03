23ca6e3d543cad8b6cf387b25ecd398253cddc3d
Meriç Kara <selam@merickara.net>
2025-07-03 21:11:57 +0300

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import redirect_router from "react-redirect-router"
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), 
+    redirect_router()
+  ],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',  // Alias for components
+      '@utils': '/src/utils',            // Alias for utility functions
+    }
+  },
+  define: {
+    'process.env.API_URL': JSON.stringify(process.env.API_URL || 'http://localhost:5000'),
+  }
+})
